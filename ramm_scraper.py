07bfbf0473e4b9429662434946c80358e131d580
mrz <moritzschwab02@gmail.com>
2022-03-02 21:59:19 +0100

    title saving

--- ramm_scraper.py
+++ ramm_scraper.py
@@ -1,12 +1,13 @@
 import os
 import requests
+import time
 from bs4 import BeautifulSoup
 import argparse
-from scripts import functions
+from scripts.functions import *
 
-parser = argparse.ArgumentParser(description='Style Transfer')
-parser.add_argument('--start', default=0, help='Starting id. Default: 0')
-parser.add_argument('--end', default=10000, help='Ending id. Default: 10.000')
+parser = argparse.ArgumentParser(description='RammsteinShop scraper.')
+parser.add_argument('--start', type=int, default=0, help='Starting id. Default: 0')
+parser.add_argument('--end', type=int, default=10000, help='Ending id. Default: 10.000')
 parser.add_argument('--new', action='store_true', help='Enable scraping new items.')
 
 args = vars(parser.parse_args())
@@ -16,3 +17,19 @@ scrape_new = args['new']
 
 BASE_URL = 'https://shop.rammstein.de/en/catalog/product_info.php?products_id='
 WHATS_NEW_URL = 'https://shop.rammstein.de/en/catalog/whats-new.html'
+
+if scrape_new:
+    pass
+else:
+    for current_id in range(start_id, end_id+1):
+        time.sleep(1)
+        url = BASE_URL + str(current_id)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        if response.status_code == 200:
+            print(f'{current_id} ok')
+            print(save_page(soup, url))
+        else:
+            print(f'{current_id} empty')
+
--- scripts/functions.py
+++ scripts/functions.py
@@ -1,15 +1,27 @@
+from msilib.schema import Error
 import os
 import requests
 from bs4 import BeautifulSoup
+import urllib3
 
-def save_page():
+BASE_URL = 'https://shop.rammstein.de/en/catalog/product_info.php?products_id='
+WHATS_NEW_URL = 'https://shop.rammstein.de/en/catalog/whats-new.html'
+
+def get_image_urls(soup):
+    img_urls = []
+    return img_urls
+
+def save_page(soup, url):
+    title = soup.find_all("li", class_="active hidden-xxs")[0].next
+    img_urls = get_image_urls(soup)
+    text = f'{url}\n\n{title}\n'
+    return 'Files written!'
+
+def save_images(urls):
     pass
 
-def save_images():
-    pass
-
-def save_description():
-    pass
+#def save_description():
+#    pass
 
 def whats_new_scraper():
     pass
