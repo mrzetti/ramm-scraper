c36f67c830f1810ecf81e69e88de654e3143d4e0
mrz <moritzschwab02@gmail.com>
2022-03-03 21:30:56 +0100

    folder creation fix

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,4 +1,3 @@
-from msilib.schema import Error
 import os
 import requests
 from bs4 import BeautifulSoup
@@ -17,17 +16,17 @@ def save_page(soup, url, id):
 
     title = soup.find_all("li", class_="active hidden-xxs")[0].next
 
-    if not os.path.exists(f'{id} {title}'):
-        os.makedirs(f'{id} {title}')
+    if not os.path.exists(f'RammsteinShop/{id} {title}'):
+        os.makedirs(f'RammsteinShop/{id} {title}')
     
     img_urls = get_image_urls(soup)
     text = f'{url}\n\n{title}\n'
 
-    file = open("demofile3.txt", "w")
+    file = open(f'RammsteinShop/{id} {title}/info.txt', 'w')
     file.write(text)
     file.close()
     
-    return 'Files written!'
+    return f'id={id}: Files written!'
 
 def save_images(urls):
     pass
